62123220ef7cf1edc0647c633c66d2b12e49c7b7
Petar Shomov <pshomov@gmail.com>
2011-12-19 08:19:18 +0000

    memory leaks fixes

--- Snitch/main.m
+++ Snitch/main.m
@@ -25,18 +25,21 @@ int main(int argc, char *argv[])
     while(TRUE) {
         NSAutoreleasePool *loopPool;
         loopPool = [[NSAutoreleasePool alloc] init];
-        int idletime = CGEventSourceSecondsSinceLastEventType(1,NX_KEYDOWN | NX_LMOUSEDOWN | NX_LMOUSEUP | NX_RMOUSEDOWN | NX_RMOUSEUP | NX_MOUSEMOVED | NX_LMOUSEDRAGGED | NX_RMOUSEDRAGGED | NX_KEYUP | NX_FLAGSCHANGED | NX_SCROLLWHEELMOVED);
+        int idletime = CGEventSourceSecondsSinceLastEventType(1,NX_KEYDOWN | NX_LMOUSEDOWN | NX_LMOUSEUP | NX_RMOUSEDOWN | NX_RMOUSEUP | NX_MOUSEMOVED | NX_LMOUSEDRAGGED | NX_RMOUSEDRAGGED | NX_KEYUP | NX_SCROLLWHEELMOVED);
         if (idletime < 60) {
-            NSDateFormatter *format1 = [[[NSDateFormatter alloc] init] autorelease];
-            NSDateFormatter *format2 = [[[NSDateFormatter alloc] init] autorelease];
+            NSDateFormatter *format1 = [[NSDateFormatter alloc] init];
+            NSDateFormatter *format2 = [[NSDateFormatter alloc] init];
             [format1 setDateFormat:@"yyyyMMdd/yyyyMMdd-HHmmss"];
             [format2 setDateFormat:@"yyyyMMdd"];
             
-            NSDate *now = [[[NSDate alloc] init] autorelease];
+            NSDate *now = [[NSDate alloc] init];
             
             NSString *fileNameString = [format1 stringFromDate:now];
             NSString *folderNameString = [format2 stringFromDate:now];
             [[NSFileManager defaultManager] createDirectoryAtPath:folderNameString withIntermediateDirectories:TRUE attributes:nil error:NULL];
+            [format1 release];
+            [format2 release];
+            [now release];
             SaveDesktop([fileNameString stringByAppendingString:@".jpg"]);
             [loopPool drain];
             
